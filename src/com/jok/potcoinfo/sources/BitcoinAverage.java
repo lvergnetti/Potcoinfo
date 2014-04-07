0b0fa534211833eb9f6858d809ef4ec71ec9c8cc
notimethanks <notimethanks@gmail.com>
2014-04-07 16:33:05 -0400

    Project added

---
+++ src/com/jok/potcoinfo/sources/BitcoinAverage.java
@@ -0,0 +1,10 @@
+package com.jok.potcoinfo.sources;
+
+import com.google.gson.annotations.SerializedName;
+
+public class BitcoinAverage {
+	
+	@SerializedName("24h_avg")
+	public double bitcoin_price;
+	
+}
---
+++ src/com/jok/potcoinfo/sources/Mintpal.java
@@ -0,0 +1,13 @@
+package com.jok.potcoinfo.sources;
+
+public class Mintpal {
+	
+	private double last_price;
+	double change;
+	public double getLast_price() {
+		return last_price;
+	}
+	public void setLast_price(double last_price) {
+		this.last_price = last_price;
+	}
+}
