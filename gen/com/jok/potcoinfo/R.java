4167c242a5587c7648ed0fd809d6a4d41485145a
notimethanks <notimethanks@gmail.com>
2014-04-07 19:27:13 -0400

    Wallet Actionbar Icon
    added

--- gen/com/jok/potcoinfo/R.java
+++ gen/com/jok/potcoinfo/R.java
@@ -173,21 +173,21 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int background_image=0x7f020003;
         public static final int btc=0x7f020004;
         public static final int default_ptr_flip=0x7f020005;
-        public static final int default_ptr_rotate=0x7f020006;
-        public static final int default_ptr_rotate3=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int indicator_arrow=0x7f020009;
-        public static final int indicator_bg_bottom=0x7f02000a;
-        public static final int indicator_bg_top=0x7f02000b;
-        public static final int leaf=0x7f02000c;
-        public static final int logo=0x7f02000d;
-        public static final int market_icon=0x7f02000e;
-        public static final int network=0x7f02000f;
-        public static final int qr_actionbar=0x7f020010;
-        public static final int qr_addresses=0x7f020011;
-        public static final int refresh_background=0x7f020012;
-        public static final int refresh_background_image=0x7f020013;
-        public static final int usd=0x7f020014;
+        public static final int default_ptr_rotate3=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int indicator_arrow=0x7f020008;
+        public static final int indicator_bg_bottom=0x7f020009;
+        public static final int indicator_bg_top=0x7f02000a;
+        public static final int leaf=0x7f02000b;
+        public static final int logo=0x7f02000c;
+        public static final int market_icon=0x7f02000d;
+        public static final int network=0x7f02000e;
+        public static final int qr_actionbar=0x7f02000f;
+        public static final int qr_addresses=0x7f020010;
+        public static final int refresh_background=0x7f020011;
+        public static final int refresh_background_image=0x7f020012;
+        public static final int usd=0x7f020013;
+        public static final int wallet_actionbar=0x7f020014;
     }
     public static final class id {
         public static final int address=0x7f05001e;
@@ -227,11 +227,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int pull_to_refresh_progress=0x7f050027;
         public static final int pull_to_refresh_sub_text=0x7f050029;
         public static final int pull_to_refresh_text=0x7f050028;
-        public static final int qr=0x7f05002a;
+        public static final int qr=0x7f05002b;
         public static final int rotate=0x7f050007;
         public static final int scanner_balance=0x7f050020;
         public static final int scrollView1=0x7f05000c;
         public static final int scrollview=0x7f05000b;
+        public static final int wallet=0x7f05002a;
         public static final int webview=0x7f05000a;
     }
     public static final class layout {
