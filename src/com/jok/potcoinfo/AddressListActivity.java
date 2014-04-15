bd2690538d0f88c54289a0a97384512289122bcb
leo <lvergnetti@gmail.com>
2014-04-15 15:29:56 -0400

    light at the end of the tunnel
    started address activity in place of scanner activity.  address activity
    is saving the address.  address list activity is reading it.

--- src/com/jok/potcoinfo/AddressListActivity.java
+++ src/com/jok/potcoinfo/AddressListActivity.java
@@ -3,6 +3,7 @@ package com.jok.potcoinfo;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.widget.ArrayAdapter;
@@ -27,37 +28,38 @@ public class AddressListActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_address_list);
-        listView = (ListView) findViewById(R.id.list);
+        
         setLayout();
-        SharedPreferences prefs = this.getSharedPreferences("AddressList",Context.MODE_PRIVATE);
-        String storedList = prefs.getString("StoredList", null);
-        String[] addresses = {};
-        if (storedList!=null){
-	        Address[] addressList = getAddressList();
-	        for(int i=0;i<addressList.length;i++){
-	        	Address address = addressList[i];
-	        	
-	        	addresses[i] = address.address;
-	        }
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-              android.R.layout.simple_list_item_1, android.R.id.text1, addresses);
-            listView.setAdapter(adapter);
-            
-            
-	        
-        }
-        else {
-        	//Do Nothing
-        }
+       // makePrefs();
+        makeListView(getAddressList());
+        
      }
 	
     public void setLayout(){
+    	setContentView(R.layout.activity_address_list);
     	//addLayout Needs LIST, SCAN BUTTON, and TYPE BUTTON
     	//
 	}
-	
+	public void makePrefs(){
+		SharedPreferences prefs = getSharedPreferences("AddressList", Context.MODE_PRIVATE);
+    	Editor e = prefs.edit();
+    	Address address = new Address();
+    	address.address = "LEOSADDRESSDOOD";
+    	address.name = "LEOSNAMEDioOOD";
+    	Address[] addresses = new Address[1];
+    	System.out.println("address array made - empty");
+    	for(int i=0; i < addresses.length; i++){
+    		addresses[i] = address; 
+    		System.out.println("address array populated at " + i);
+    	}
+    	
+    	Gson gson = new Gson();
+    	String addressJson = gson.toJson(addresses);
+    	System.out.println("addressJson" + addressJson);
+    	e.putString("StoredList", addressJson);
+    	e.commit();
+    	System.out.println("Preference saved");
+	}
 	public Address[] getAddressList(){
 		Address[] addressList={};
 		SharedPreferences prefs = getSharedPreferences("AddressList", Context.MODE_PRIVATE);
@@ -66,14 +68,24 @@ public class AddressListActivity extends Activity{
 			GsonBuilder gsonBuilder = new GsonBuilder();
 			Gson gson = gsonBuilder.create();
 			addressList = gson.fromJson(storedList, Address[].class);
+			
 		}
+		
 		return addressList;
 	}
 	
-	public void addItemToListView(Address address){
-		String name;
-		name = address.name;
+	public void makeListView(Address[] addresses){
+		String[] stringAddresses = new String[addresses.length];
+		String[] stringNames = new String[addresses.length];
+		for(int i=0;i<addresses.length;i++){
+			stringAddresses[i] = addresses[i].address;
+			stringNames[i] = addresses[i].name;
+		}
+		listView = (ListView) findViewById(R.id.list);
 		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, android.R.id.text1, stringAddresses);
+        listView.setAdapter(adapter);
 		
 	}
 	
